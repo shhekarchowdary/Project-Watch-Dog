383c6cd4f11218a4a08b202b9d72da484ffc79ed
Soma Sekhar Anaparthi <shhekarchowdary@gmail.com>
2022-02-13 04:05:36 -0500

    Project Creation done - without adding it to user

--- User.js
+++ User.js
@@ -1,10 +1,11 @@
 ﻿class User {
 
-    constructor(uName, uMailId, uPassword, uRole){
+    constructor(uName, uMailId, uPassword, uRole, uProjectIds){
         this.uName = uName;
         this.uMailId = uMailId;
         this.uPassword = uPassword;
         this.uRole = uRole;
+        this.uProjectIds = uProjectIds;
     }
 
     
